9aa90d27a18994fd1a1aa38ddf667c4c8a2dcdc7
UlloaN <32268457+UlloaN@users.noreply.github.com>
2018-07-12 12:44:16 -0500

    Create 01_control_structures.py

---
+++ 01_control_structures.py
@@ -0,0 +1,11 @@
+import random
+
+points=0
+counts=0
+
+for i in range(100):
+    while points < 100:
+        point_once=int(random.random()*6+1)
+        points += point_once
+        counts += 1
+    print(point_once,points,counts)
