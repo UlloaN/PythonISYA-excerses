114eb2c42a1e0264650b5481b94640070fe5321c
UlloaN <32268457+UlloaN@users.noreply.github.com>
2018-07-12 15:44:08 -0500

    Update 01_control_structures.py

--- 01_control_structures.py
+++ 01_control_structures.py
@@ -1,12 +1,18 @@
 import random
-
-points=0
-counts=0
-
-for i in range(100):
+import math as mt
+#a=[]
+c=0
+for i in list(range(100)):
+    points=0
+    counts=0
     while points < 100:
         point_once=int(random.random()*6+1)
         points += point_once
         counts += 1
-    points += point_once
-    print(point_once,points,counts)
+    i += 1 #for count the iterations
+#    a.append(counts) to save in a list
+    c += counts
+average=c/i
+average=int(average)
+#print(point_once,points,counts,c,i,average) # to test
+print('The average of have a minimun 100 points when you thrown a die is:', average, 'times') 
