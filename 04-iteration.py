9cc616d34c1a6ae7595dcc5f9e6991aac7737486
UlloaN <32268457+UlloaN@users.noreply.github.com>
2018-07-12 15:07:29 -0500

    Update 04-iteration.py

--- 04-iteration.py
+++ 04-iteration.py
@@ -1,7 +1,10 @@
+import random
+
 a1=0
-
-In [9]: for i in range(100):
-   ...:     a=random.random()
-   ...:     a1 += a  # is same like a1=a1+a
-   ...:     print(a,a1)
-
+count=0
+for i in range(100):
+   a=random.random()
+   a1 += a  # is same like a1=a1+a
+   count += 1
+   #print(a,a1,count)
+print('the sum of 100 random numbers is ', a1)
